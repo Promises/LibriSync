707f51be32a820e98cf205fc640e3cfe0d3ec896
Henning Berge <henning.ber@gmail.com>
2025-11-02 23:08:01 +0100

    bump version number

--- app.config.js
+++ app.config.js
@@ -2,7 +2,7 @@ export default {
   expo: {
     name: "LibriSync",
     slug: "librisync",
-    version: "0.0.1",
+    version: "0.0.2",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "dark",
